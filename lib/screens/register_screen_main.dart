f4d5a144fed2f6a455e0168664e5243825bc53fd
erum <heartof.effort@gmail.com>
2023-09-04 16:31:36 +0700

    init rigis_main_screen

---
+++ lib/screens/register_screen_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegisterMainScreen extends StatelessWidget {
+  const RegisterMainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
