ad7552061245f33d20b132b596d58b1a8baec054
neqq <supitcha.psr@gmail.com>
2023-09-06 14:28:24 +0700

    create info card

---
+++ lib/screens/User/info_appointment.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:telephy/utils/config.dart';
+import 'package:telephy/widgets/psychologist_card.dart';
+
+class InfoAppointment extends StatelessWidget {
+  const InfoAppointment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Config.baseColor,
+        elevation: 4,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            PhychologistCard(
+              psychologistName: 'somchai',
+              workplace: 'F',
+              ratePerHour: '350',
+              setBorderCardBottomLeft: false,
+              setBorderCardBottomRight: false,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 24, right: 24),
+              height: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
+                color: Config.lighterToneColor,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                "psychologistName",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Icon(
+                                Icons.location_on,
+                                size: 20,
+                              ),
+                              Text("workplace")
+                            ],
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              SizedBox(
+                                width: 8,
+                              ),
+                              FaIcon(
+                                FontAwesomeIcons.stethoscope,
+                                size: 17,
+                              ),
+                              Text("\$ บาท / 1 ชั่วโมง")
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
