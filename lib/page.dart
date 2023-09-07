0b80493b8aa0fcbdb9cbbdaedb9c2587077f3473
Fair <pattaradanai4290@gmail.com>
2023-09-07 22:16:09 +0700

    edit page route to GetPage

--- lib/page.dart
+++ lib/page.dart
@@ -1,12 +1,37 @@
 import 'package:get/get.dart';
+import 'package:telephy/main_layout.dart';
+import 'package:telephy/screens/booking_screen.dart';
+import 'package:telephy/screens/login_screen.dart';
+import 'package:telephy/screens/register_screen.dart';
+import 'package:telephy/screens/video_main.dart';
 import 'pages/message/index.dart';
 
 class AppPages {
   static final List<GetPage> routes = [
     GetPage(
+      name: '/main',
+      page: () => MainLayout(),
+    ),
+    GetPage(
       name: '/message',
       page: () => MessagePage(),
       binding: MessageBinding(),
+    ),
+    GetPage(
+      name: '/video',
+      page: () => VideoScreen(),
+    ),
+    GetPage(
+      name: '/register',
+      page: () => RegisterScreen(),
+    ),
+    GetPage(
+      name: '/booking',
+      page: () => BookingScreen(),
+    ),
+    GetPage(
+      name: '/login',
+      page: () => LoginPage(),
     ),
   ];
 }
