9195b6dd355912c40fb9edcd1d219f84e8910c08
erum <heartof.effort@gmail.com>
2023-09-07 22:52:05 +0700

    update

--- lib/screens/register_main_screen.dart
+++ lib/screens/register_main_screen.dart
@@ -1,6 +1,7 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:telephy/widgets/regist_app_bar.dart';
+import 'package:telephy/widgets/regist_bt.dart';
+import 'package:telephy/widgets/regist_text_field.dart';
 
 class RegisterMainScreen extends StatelessWidget {
   const RegisterMainScreen({super.key});
@@ -12,83 +13,9 @@ class RegisterMainScreen extends StatelessWidget {
     final TextEditingController passwordController = TextEditingController();
     final TextEditingController cfPasswordController = TextEditingController();
 
-    const boderInputStyle = OutlineInputBorder(
-      borderSide: BorderSide(
-        style: BorderStyle.solid,
-        width: 2,
-      ),
-      borderRadius: BorderRadius.all(Radius.circular(30)),
-    );
-
-    InputDecoration inputDec(String hintText) {
-      return InputDecoration(
-        floatingLabelBehavior: FloatingLabelBehavior.never,
-        hintText: hintText,
-        hintStyle: const TextStyle(
-          color: Colors.grey,
-          fontSize: 14,
-        ),
-        filled: true,
-        fillColor: Colors.white,
-        focusedBorder: boderInputStyle,
-        enabledBorder: boderInputStyle,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 20)
-      );
-    }
-
-    const labelxStyle = TextStyle(
-      fontSize: 16,
-      color: Colors.black,
-      fontWeight: FontWeight.bold,
-    );
-
-    Container nextButton = Container(
-      margin: const EdgeInsets.only(top: 45),
-      width: 240,
-      height: 35,
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          colors: [Color.fromRGBO(134, 210, 252, 1), Color.fromRGBO(204, 173, 249, 1)]
-        ),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(30))
-          ),
-        ),
-        onPressed: () {}, 
-        child: const Text("ถัดไป")),
-    );
 
     return Scaffold(
-      appBar: AppBar(
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Color.fromRGBO(124, 129, 255, 1), Color.fromRGBO(174, 178, 253, 1), Color.fromRGBO(215, 216, 255, 0)]),
-          ),
-        ),
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        foregroundColor: Colors.black,
-        title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.arrow_back_ios_new_rounded)),
-          const Text("ลงทะเบียน", style: TextStyle(fontSize: 28),),
-          const SizedBox(width: 30,)
-        ],
-      )),
+      appBar: const RegisterAppBar(title: "ลงทะเบียน"),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(30),
@@ -98,45 +25,34 @@ class RegisterMainScreen extends StatelessWidget {
               Container(
                   height: 38,
                   color: Colors.black,
-                  margin: const EdgeInsets.only(bottom: 30)), // state container
-              const SizedBox(
-                  height: marginBtwTF, child: Text("ชื่อผู้ใช้", style: labelxStyle)),
-              TextField(
-                controller: usernameController,
-                keyboardType: TextInputType.name,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: inputDec("Username"),
+                  margin: const EdgeInsets.only(bottom: 30)
+              ), // state container
+              RegistTextField(
+                title: "ชื่อผู้ใช้", 
+                hintText: "Username", 
+                type: TextInputType.name,
+                tfController: usernameController,
               ),
               const SizedBox(
-                height: 20,
+                height: marginBtwTF,
+              ),
+              RegistTextField(
+                title: "รหัสผ่าน", 
+                hintText: "Password", 
+                type: TextInputType.visiblePassword, 
+                tfController: passwordController,
               ),
               const SizedBox(
-                  height: marginBtwTF, child: Text("รหัสผ่าน", style: labelxStyle)),
-              TextField(
-                controller: passwordController,
-                keyboardType: TextInputType.visiblePassword,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: inputDec("Password"),
+                height: marginBtwTF,
               ),
-              const SizedBox(
-                height: 20,
+              RegistTextField(
+                title: "ยืนยันรหัสผ่าน", 
+                hintText: "Confirm Password", 
+                type: TextInputType.name,
+                tfController: cfPasswordController,
               ),
-              const SizedBox(
-                  height: marginBtwTF, child: Text("ยืนยันรหัสผ่าน", style: labelxStyle)),
-              TextField(
-                controller: cfPasswordController,
-                keyboardType: TextInputType.visiblePassword,
-                style: const TextStyle(
-                  color: Colors.black,
-                ),
-                decoration: inputDec("Confirm Password"),
-              ),
-              Center(
-                child: nextButton,
+              const Center(
+                child: RegistBT(titleBT: "ถัดไป"),
               )
             ],
           ),
