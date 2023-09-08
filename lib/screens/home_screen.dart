c490cd622e36bf2ed6554fe90a9fc472a0ea72fa
aotsurasak46 <aotsurasak46@gmail.com>
2023-09-08 13:40:25 +0700

    edit psy card to add image

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,1 @@
 
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Home"),
-        ],
-      ),
-    );
-  }
-}
