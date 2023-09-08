75036c688b6e02c4816e4447a32ae90d3e889871
areeyaswnth <teenteentantan@gmail.com>
2023-09-08 16:41:59 +0700

    TimeSlot update

--- lib/screens/time_slot_screen.dart
+++ lib/screens/time_slot_screen.dart
@@ -3,22 +3,28 @@ import 'package:telephy/widgets/calender.dart';
 import 'package:telephy/widgets/timeSlot.dart';
 
 class TimeSlot extends StatefulWidget {
-  const TimeSlot({super.key});
+  const TimeSlot({Key? key}) : super(key: key);
 
   @override
-  State<TimeSlot> createState() => _TimeSlotState();
+  _TimeSlotState createState() => _TimeSlotState();
 }
 
 class _TimeSlotState extends State<TimeSlot> {
+  DateTime currentDate = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
           Expanded(
-            child: Calendar(),
+            child: Calendar(), // เรียกใช้ Calendar() จากวิดเจ็ต Calendar
           ),
-          Expanded(child: TimeSlotTable())
+          Expanded(
+            child: TimeSlotTable(
+                currentDate:
+                    currentDate), // ส่งค่า currentDate ไปยัง TimeSlotTable
+          ),
         ],
       ),
     );
