887f28037a10268f0ae347eea6068e253670aaa2
BubuSnk <Suppawih2905@gmail.com>
2023-09-13 19:12:10 +0700

    message button

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
           type: BottomNavigationBarType.fixed,
         ),
       ),
-      initialRoute: 'register',
+      initialRoute: '/message',
       getPages: AppPages.routes,
     );
   }
