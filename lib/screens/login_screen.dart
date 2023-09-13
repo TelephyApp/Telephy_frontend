a6ece2209a9c0bb7e04b790d5d069d0952c8b420
ChawinSriviset <64011091@kmitl.ac.th>
2023-09-13 13:01:50 +0700

    update firebase

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -25,131 +25,132 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     const double marginbtw = 20;
-    double screenWidth = MediaQuery.of(context).size.width;
     return Container(
-        // wrap Container ไว้เพื่อทำสีแบบ gradient
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-              Config.mainColor1,
-              Config.mainColor2,
-            ])),
-        child: Scaffold(
-          // นี่คือตัวจัด layout หลัก
-          backgroundColor: Colors.transparent,
-          body: SafeArea(
-            // wrap Column ไว้ทำให้ไม่ทับขอบบน
-            child: Center(
-              child: Column(
-                children: [
-                  // sizedbox = ทำที่ว่าง
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  // logo
-                  Icon(
-                    Icons.person_add,
-                    size: 100,
-                    color: Colors.white,
-                  ),
+      // wrap Container ไว้เพื่อทำสีแบบ gradient
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+            Config.mainColor1,
+            Config.mainColor2,
+          ])),
+      child: Scaffold(
+        // นี่คือตัวจัด layout หลัก
+        backgroundColor: Colors.transparent,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // sizedbox = ทำที่ว่าง
+            const SizedBox(
+              height: 100,
+            ),
+            // logo
+            Icon(
+              Icons.person_add,
+              size: 100,
+              color: Colors.white,
+            ),
 
-                  const SizedBox(
-                    height: 30,
-                  ),
+            const SizedBox(
+              height: 30,
+            ),
 
-                  // top text
-                  Text(
-                    'เข้าสู่ระบบ',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-
-                  const SizedBox(
-                    height: marginbtw,
-                  ),
-
-                  // username textfield
-                  MyTextField(
-                    controller: _usernameController,
-                    prefixIcon: Icon(Icons.person),
-                    hintText: 'ชื่อผู้ใช้',
-                    obscureText: false,
-                  ),
-
-                  const SizedBox(
-                    height: marginbtw,
-                  ),
-
-                  // password textfield
-                  MyTextField(
-                    controller: _passwordController,
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: 'รหัสผ่าน',
-                    obscureText: true,
-                  ),
-
-                  const SizedBox(
-                    height: marginbtw,
-                  ),
-
-                  // ปุ่ม sign in
-                  LoginButton(
-                    text: 'เข้าสู่ระบบ',
-                    onTap: signUserIn,
-                  ),
-
-                  const SizedBox(
-                    height: marginbtw,
-                  ),
-
-                  // google sign in button
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SquareTile(imagePath: 'assets/images/google.png'),
-                    ],
-                  ),
-
-                  const SizedBox(
-                    height: marginbtw,
-                  ),
-
-                  // register text
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('ยังไม่มีบัญชีผู้ใช้?'),
-                      const SizedBox(
-                        width: 4,
-                      ),
-                      Text(
-                        'ลงทะเบียน',
-                        style: TextStyle(
-                            decoration: TextDecoration.underline,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-
-                  Spacer(),
-
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 198,
-                    child: Image.asset(
-                      'assets/images/cloud.png',
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                ],
+            // top text
+            Text(
+              'เข้าสู่ระบบ',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontWeight: FontWeight.normal,
               ),
             ),
-          ),
-        ));
+
+            const SizedBox(
+              height: marginbtw,
+            ),
+
+            // username textfield
+            MyTextField(
+              controller: _usernameController,
+              prefixIcon: Icon(Icons.person),
+              hintText: 'ชื่อผู้ใช้',
+              obscureText: false,
+            ),
+
+            const SizedBox(
+              height: marginbtw,
+            ),
+
+            // password textfield
+            MyTextField(
+              controller: _passwordController,
+              prefixIcon: Icon(Icons.lock),
+              hintText: 'รหัสผ่าน',
+              obscureText: true,
+            ),
+
+            const SizedBox(
+              height: marginbtw,
+            ),
+
+            // ปุ่ม sign in
+            LoginButton(
+              text: 'เข้าสู่ระบบ',
+              onTap: signUserIn,
+            ),
+
+            const SizedBox(
+              height: marginbtw,
+            ),
+
+            // google sign in button
+            GestureDetector(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'assets/images/google.png'),
+                ],
+              ),
+              onTap: () {
+                print("login");
+              },
+            ),
+
+            const SizedBox(
+              height: marginbtw,
+            ),
+
+            // register text
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('ยังไม่มีบัญชีผู้ใช้?'),
+                const SizedBox(
+                  width: 4,
+                ),
+                Text(
+                  'ลงทะเบียน',
+                  style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+
+            Spacer(),
+
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 198,
+              child: Image.asset(
+                'assets/images/cloud.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import agora_rtc_engine
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AgoraRtcEnginePlugin.register(with: registry.registrar(forPlugin: "AgoraRtcEnginePlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
