ddbc2a5159efce5b61b399a177e4753b06d98a2a
areeyaswnth <teenteentantan@gmail.com>
2023-09-19 10:35:44 +0700

    booking dai laew yay

--- lib/widgets/timeSlot.dart
+++ lib/widgets/timeSlot.dart
@@ -29,146 +29,181 @@ class _TimeSlotTableState extends State<TimeSlotTable> {
 
     final lastThreeDays = nextTwoDays;
 
-    return Container(
-      padding: const EdgeInsets.only(left: 10, right: 10),
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [Color(0xFFFEFEFE), Color(0xFFFEFEFE), Color(0xFFDDDEFC)],
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-        ),
-      ),
-      child: Column(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: const Color(0xFF0F1B2D), width: 0),
-              color: Config.darkerToneColor,
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
+    return Stack(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFFFEFEFE), Color(0xFFFEFEFE), Color(0xFFDDDEFC)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
             ),
-            child: Container(
-              height: 100,
-              padding: const EdgeInsets.all(11),
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 129, 129, 129),
-                    offset: Offset(0, 2),
-                    blurRadius: 4,
-                  )
-                ],
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                gradient: LinearGradient(
-                  colors: [Color(0xFFB2DDFD), Color(0xFFB2B4FE)],
-                  begin: Alignment.bottomCenter,
-                  end: Alignment.topCenter,
+          ),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: const Color(0xFF0F1B2D), width: 0),
+                  color: Config.darkerToneColor,
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
                 ),
-              ),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: SizedBox(
-                      width: 20,
+                child: Container(
+                  height: 100,
+                  padding: const EdgeInsets.all(11),
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(255, 129, 129, 129),
+                        offset: Offset(0, 2),
+                        blurRadius: 4,
+                      )
+                    ],
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    gradient: LinearGradient(
+                      colors: [Color(0xFFB2DDFD), Color(0xFFB2B4FE)],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
                     ),
                   ),
-                  for (var dayIndex = 0;
-                      dayIndex < lastThreeDays.length;
-                      dayIndex++)
-                    Expanded(
-                      child: Center(
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              DateFormat('EEE').format(lastThreeDays[dayIndex]),
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: dayIndex == 0
-                                    ? const Color(0xFFFEFEFE)
-                                    : const Color(0xFF0F1B2D),
-                              ),
-                            ),
-                            Container(
-                              height: 40,
-                              width: 40,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50),
-                                color: dayIndex == 0
-                                    ? const Color(0xFF0F1B2D)
-                                    : const Color.fromARGB(0, 254, 254, 254),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  DateFormat('dd')
+                  child: Row(
+                    children: [
+                      for (var dayIndex = 0;
+                          dayIndex < lastThreeDays.length;
+                          dayIndex++)
+                        Expanded(
+                          child: Center(
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  DateFormat('EEE')
                                       .format(lastThreeDays[dayIndex]),
                                   style: TextStyle(
-                                    fontSize: 25,
+                                    fontSize: 12,
                                     color: dayIndex == 0
                                         ? const Color(0xFFFEFEFE)
                                         : const Color(0xFF0F1B2D),
                                   ),
                                 ),
-                              ),
+                                Container(
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: dayIndex == 0
+                                        ? const Color(0xFF0F1B2D)
+                                        : const Color.fromARGB(
+                                            0, 254, 254, 254),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      DateFormat('dd')
+                                          .format(lastThreeDays[dayIndex]),
+                                      style: TextStyle(
+                                        fontSize: 25,
+                                        color: dayIndex == 0
+                                            ? const Color(0xFFFEFEFE)
+                                            : const Color(0xFF0F1B2D),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                ],
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: widget.numberOfHours,
-              itemBuilder: (BuildContext context, int hourIndex) {
-                final hour = hourIndex.toString().padLeft(2, '0');
-                return Row(
-                  children: [
-                    Expanded(
-                      child: TimeSlot(hour: hour),
-                    ),
-                    for (var dayIndex = 0;
-                        dayIndex < lastThreeDays.length;
-                        dayIndex++)
-                      Expanded(
-                        child: HourlySlot(
-                          day: lastThreeDays[dayIndex],
-                          hour: hour,
-                          isTapped: isSlotTapped[dayIndex]
-                              [hourIndex], // Pass tapped state
-                          onTap: () {
-                            // Toggle tapped state for this specific day and hour
-                            setState(() {
-                              isSlotTapped[dayIndex][hourIndex] =
-                                  !isSlotTapped[dayIndex][hourIndex];
-                            });
-                          },
+              Expanded(
+                child: ListView.builder(
+                  itemCount: widget.numberOfHours,
+                  itemBuilder: (BuildContext context, int hourIndex) {
+                    final hour = hourIndex.toString().padLeft(2, '0');
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: TimeSlot(
+                            hour: hour,
+                            isBooking: isBooking, // Pass isBooking state
+                          ),
                         ),
-                      ),
-                  ],
-                );
-              },
+                        for (var dayIndex = 0;
+                            dayIndex < lastThreeDays.length;
+                            dayIndex++)
+                          Expanded(
+                            child: HourlySlot(
+                              day: lastThreeDays[dayIndex],
+                              hour: hour,
+                              isTapped: isSlotTapped[dayIndex]
+                                  [hourIndex], // Pass tapped state
+                              onTap: () {
+                                if (isBooking) {
+                                  // Toggle tapped state for this specific day and hour
+                                  setState(() {
+                                    isSlotTapped[dayIndex][hourIndex] =
+                                        !isSlotTapped[dayIndex][hourIndex];
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                      ],
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          bottom: 20,
+          right: 20, // Set the button position to the bottom right
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              
+              shadowColor: Colors.transparent,
+              //make color or elevated button transparent
             ),
+            onPressed: () {
+              setState(() {
+                isBooking = !isBooking;
+              });
+            },
+            child: Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF86D2FC), Color(0xFFCCADF9)],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    )),
+                child: Icon(isBooking ? Icons.done : Icons.add)),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
 
 class TimeSlot extends StatelessWidget {
   final String hour;
+  final bool isBooking;
 
-  const TimeSlot({Key? key, required this.hour}) : super(key: key);
+  const TimeSlot({Key? key, required this.hour, required this.isBooking})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -180,7 +215,10 @@ class TimeSlot extends StatelessWidget {
           ),
           Text(
             '$hour:00',
-            style: const TextStyle(fontSize: 16, color: Color(0xFF0F1B2D)),
+            style: TextStyle(
+              fontSize: 16,
+              color: isBooking ? Colors.grey : Color(0xFF0F1B2D),
+            ),
           ),
           const SizedBox(
             height: 20,
@@ -295,4 +333,12 @@ class HourlySlot extends StatelessWidget {
             ),
           );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: TimeSlotTable(currentDate: DateTime.now()),
+    ),
+  ));
 }
