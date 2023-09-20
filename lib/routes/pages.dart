7174b9e6e10e93c40bb1c83a598f490f34b91b66
neqq <supitcha.psr@gmail.com>
2023-09-20 17:54:11 +0700

    added user navbar

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,15 +1,20 @@
 import 'package:get/get.dart';
-import 'package:telephy/main_layout.dart';
+import 'package:telephy/psych_layout.dart';
 import 'package:telephy/pages/message/chat/index.dart';
 import 'package:telephy/screens/login_screen.dart';
 import 'package:telephy/screens/register_screen.dart';
 import 'package:telephy/pages/message/index.dart';
+import 'package:telephy/user_layout.dart';
 
 class AppPages {
   static final List<GetPage> routes = [
     GetPage(
-      name: '/main',
-      page: () => MainLayout(),
+      name: '/main-user',
+      page: () => UserLayout(),
+    ),
+    GetPage(
+      name: '/main-psych',
+      page: () => PsychLayout(),
     ),
     GetPage(
       name: '/message',
