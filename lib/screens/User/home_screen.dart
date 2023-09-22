ac12e3c9c968ffda01d674b3b2e13c77d24a4bf9
areeyaswnth <teenteentantan@gmail.com>
2023-09-22 10:29:21 +0700

    move folder

--- lib/screens/User/home_screen.dart
+++ lib/screens/User/home_screen.dart
@@ -11,19 +11,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD:lib/screens/home_screen.dart
-    return const Padding(
-      padding: EdgeInsets.all(20),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Home"),
-          
-        ],
-      ),
-    );
-=======
     return const Placeholder();
->>>>>>> 95c1daa9d2be5831a2c1a13a95b74bb27adf4d62:lib/screens/User/home_screen.dart
   }
 }
