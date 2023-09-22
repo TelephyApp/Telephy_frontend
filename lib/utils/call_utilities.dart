da82a6b6dde651019e562795003481653928fedb
BubuSnk <Suppawih2905@gmail.com>
2023-09-22 18:36:12 +0700

    vdocall

--- lib/utils/call_utilities.dart
+++ lib/utils/call_utilities.dart
@@ -1,36 +1,40 @@
-// import 'dart:math';
+import 'dart:math';
 
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:telephy/model/call.dart';
-// import 'package:telephy/videocall_module/call_method.dart';
-// import 'package:telephy/videocall_module/videocall_screen_test.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:telephy/model/call.dart';
+import 'package:telephy/videocall_module/call_method.dart';
+import 'package:telephy/videocall_module/videocall_screen_test.dart';
 
-// class CallUtils {
-//   static final CallMethods callMethods = CallMethods();
+class CallUtils {
+  static final CallMethods callMethods = CallMethods();
+  static dial({required User from, required User to, context}) async {
+    // Users users = GetStateUpdaterMixin;
 
-//   static dial({User from, User to, context}) async {
-//     Call call = Call(
-//       callerId: from.uid,
-//       callerName: from.name,
-//       callerPic: from.profilePhoto,
-//       receiverId: to.uid,
-//       receiverName: to.name,
-//       receiverPic: to.profilePhoto,
-//       channelId: Random().nextInt(1000).toString(),
-//     );
+    Call call = Call(
+      callerId: from.uid,
+      callerName: from.name,
+      callerPic: from.profilePhoto,
+      
+      receiverId: to.uid,
+      receiverName: to.name,
+      receiverPic: to.profilePhoto,
+      channelId: Random().nextInt(1000).toString(),
+      hasDialled: true,
+    );
 
-//     bool callMade = await callMethods.makeCall(call: call);
+    bool callMade = await callMethods.makeCall(call: call);
 
-//     call.hasDialled = true;
+    // call.hasDialled = true;
 
-//     if (callMade) {
-//       Navigator.push(
-//           context,
-//           MaterialPageRoute(
-//             builder: (context) => CallScreen(call: call),
-//           ));
-//     }
-//   }
-// }
+    if (callMade) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CallScreen(call: call),
+          ));
+    }
+  }
+}
