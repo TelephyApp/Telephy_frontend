8cfd8f3eb4553bb1c9cf18f88b2ab4a331edf2f4
Fair <pattaradanai4290@gmail.com>
2023-10-03 14:31:10 +0700

    change field in Message model

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -2,9 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Message {
   final String senderId;
-  final String senderEmail;
   final String recieverId;
-  final String recieverEmail;
   final String message;
   final Timestamp timestamp;
   final String type;
@@ -12,9 +10,7 @@ class Message {
 
   Message({
     required this.senderId,
-    required this.senderEmail,
     required this.recieverId,
-    required this.recieverEmail,
     required this.message,
     required this.timestamp,
     required this.type,
@@ -25,9 +21,7 @@ class Message {
   Map<String, dynamic> toMap() {
     return {
       'sender_id': senderId,
-      'sender_email': senderEmail,
       'reciever_id': recieverId,
-      'reciever_email': recieverEmail,
       'message': message,
       'timestamp': timestamp,
       'type': type,
@@ -38,9 +32,7 @@ class Message {
   factory Message.fromMap(Map<String, dynamic> map) {
     return Message(
       senderId: map['sender_id'] ?? '',
-      senderEmail: map['sender_email'] ?? '',
       recieverId: map['reciever_id'] ?? '',
-      recieverEmail: map['reciever_email'] ?? '',
       message: map['message'] ?? '',
       timestamp: map['timestamp'] ?? '',
       type: map['type'] ?? '',
