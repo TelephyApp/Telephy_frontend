5fa780b90e3ef421fc93a30d5346399499e53918
aotsurasak46 <aotsurasak46@gmail.com>
2023-10-11 10:49:38 +0700

    fetch ui

--- lib/widgets/card_appointment/upcoming_card.dart
+++ lib/widgets/card_appointment/upcoming_card.dart
@@ -6,143 +6,148 @@ class UpcomingCard extends StatelessWidget {
     required this.name,
     required this.detail,
     required this.dateTime,
+    required this.onclick,
     super.key,
   });
 
   final String name;
   final String detail;
   final String dateTime;
+  final Function() onclick;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Config.mainColor2,
-                Config.mainColor1,
-              ],
-              begin: Alignment.centerLeft,
-              end: Alignment.centerRight,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.25),
-                spreadRadius: 1,
-                blurRadius: 3,
-                offset: Offset(0, 2),
+    return GestureDetector(
+      onTap: onclick,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Config.mainColor2,
+                  Config.mainColor1,
+                ],
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
               ),
-            ],
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 12,
-              horizontal: 24,
-            ),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'การนัดหมายที่กำลังมาถึง',
-                      style: TextStyle(
-                        color: Config.darkerToneColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.access_time,
-                      size: 24,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      '$dateTime',
-                      style: TextStyle(
-                        color: Config.darkerToneColor,
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.25),
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                  offset: Offset(0, 2),
                 ),
               ],
-            ),
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: Config.baseColor,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.withOpacity(0.25),
-                spreadRadius: 1,
-                blurRadius: 3,
-                offset: Offset(0, 2),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
               ),
-            ],
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 12,
+                horizontal: 24,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'การนัดหมายที่กำลังมาถึง',
+                        style: TextStyle(
+                          color: Config.darkerToneColor,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time,
+                        size: 24,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '$dateTime',
+                        style: TextStyle(
+                          color: Config.darkerToneColor,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Icon(
-                      Icons.image,
-                      color: Colors.grey[400],
-                      size: 80,
-                    ),
-                    SizedBox(width: 15),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '$name',
-                            style: TextStyle(
-                              color: Config.darkerToneColor,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          Text(
-                            'Detail : $detail',
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w300,
-                            ),
-                            softWrap: true,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+          Container(
+            decoration: BoxDecoration(
+              color: Config.baseColor,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.25),
+                  spreadRadius: 1,
+                  blurRadius: 3,
+                  offset: Offset(0, 2),
                 ),
               ],
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.image,
+                        color: Colors.grey[400],
+                        size: 80,
+                      ),
+                      SizedBox(width: 15),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '$name',
+                              style: TextStyle(
+                                color: Config.darkerToneColor,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            Text(
+                              'Medical Condition : $detail',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w300,
+                              ),
+                              softWrap: true,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
